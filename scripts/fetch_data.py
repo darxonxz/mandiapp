918fc6170c102da9c50ed89344914fdd355d3217
darxonxz <darxonxz@outlook.com>
2025-12-26 14:15:41 +0530

    Add files via upload
    For fetching new files

---
+++ scripts/fetch_data.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import requests
+import os
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    warnings.warn("This is hidden")
+
+url = "https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070" # mandi api
+
+params = {
+    "api-key": "579b464db66ec23bdd000001683b398a0bdd40066aefc6ace98749c7",
+    "format": "json",
+    "limit": 10000
+}
+
+response = requests.get(url, params)
+
+# Check status
+print(response.status_code)
+
+# Convert JSON → Python object
+data = response.json()
+
+# first 5 records
+new_df = pd.DataFrame(data["records"]).reset_index()
+new_df.head(2)
+
+
+# In[2]:
+
+
+new_df.tail(2)
+
+
+DATA_FILE = "data/market_data_master.csv"
+os.makedirs("data", exist_ok=True)
+
+# In[3]:
+
+# load old data
+if os.path.exists(DATA_FILE):
+    old_df = pd.read_csv(DATA_FILE)
+    final_df = pd.concat([old_df, new_df], ignore_index=True)
+    
+else:
+    final_df = new_df
+
+final_df.to_csv(DATA_FILE, index=False)
