4aedf392c871327855cffe2ab06a8974c138afc5
darxonxz <darxonxz@outlook.com>
2025-12-26 13:09:23 +0530

    Add files via upload

---
+++ pythondep.bat
@@ -0,0 +1,12 @@
+echo@
+pip install openpyxl
+pip install -U scikit-learn
+pip install seaborn
+python.exe -m pip install --upgrade pip
+pip install --upgrade ipython ipykernel jupyter_client jupyter_core jupyter_server jupyterlab nbclient nbconvert nbformat notebook qtconsole traitlets
+pip install pypi
+pip install ipyml
+pip install ipywidgets
+pip install nbconvert
+pip install --upgrade streamlit
+pip install --upgrade plotly
